df321d2bf09f13011dba074d2dd4d7c477283f7c
Данковцев Александр Николаевич <a.dankovtsev@efko.ru>
2023-08-17 09:05:01 +0300

    Добавлено в выводе CLI описание общие свойства

--- src/Console/Commands/InfoCommand.php
+++ src/Console/Commands/InfoCommand.php
@@ -15,6 +15,7 @@ class InfoCommand implements ConsoleCommandInterface
     private static string $signature = 'info {?commandName:имя команды}';
     private static string $description = 'Вывод информации о доступных командах';
     private static bool $hidden = true;
+    private array $commonProperty = ['--interactive' => 'опция "Интерактивный режим"'];
 
     public function __construct(
         private readonly ConsoleInputInterface $input,
@@ -58,66 +59,73 @@ class InfoCommand implements ConsoleCommandInterface
         }
 
         $commandName = $definition->commandName;
-
-        echo "\033[34mЭФКО Фреймворк 0.0.1\033[0m" . PHP_EOL;
-        echo PHP_EOL;
-        echo "\033[33mФреймворк создан разработчиками компании ЭФКО Цифровые решения.\033[0m" . PHP_EOL; //желтый
-        echo "\033[33mЯвляется платформой для изучения базового поведения приложения созданного на PHP.\033[0m" . PHP_EOL; //желтый
-        echo "\033[33mФреймворк не является production-ready реализацией и не предназначен для коммерческого использования\033[0m" . PHP_EOL; //желтый
-        echo  PHP_EOL;
+        $this->output->stdout("\033[34mЭФКО Фреймворк 0.0.1\033[0m" . PHP_EOL);
+        $this->output->stdout("\033[34mЭФКО Фреймворк 0.0.1\033[0m" . PHP_EOL);
+        $this->output->stdout(PHP_EOL);
+        $this->output->stdout("\033[33mФреймворк создан разработчиками компании ЭФКО Цифровые решения.\033[0m" . PHP_EOL); //желтый
+        $this->output->stdout("\033[33mЯвляется платформой для изучения базового поведения приложения созданного на PHP.\033[0m" . PHP_EOL); //желтый
+        $this->output->stdout("\033[33mФреймворк не является production-ready реализацией и не предназначен для коммерческого использования\033[0m" . PHP_EOL); //желтый
+        $this->output->stdout(  PHP_EOL);
 
         $arguments = $definition->arguments;
         $options = $definition->options;
 
-        echo "\033[32mВызов:\033[0m" . PHP_EOL;
+        foreach ($this->commonProperty as $property => $description) {
+            $this->output->stdout("\033[34mОбщие опции:\033[0m" . PHP_EOL . "\033[32m".$property ."\033[0m". " : ". $description. PHP_EOL);
+        }
 
-        echo  "$commandName";
+        $this->output->stdout(PHP_EOL);
+
+        $this->output->stdout("\033[32mВызов:\033[0m" . PHP_EOL);
+
+        $this->output->stdout("$commandName");
 
         if (count($arguments) > 0) {
             foreach ($arguments as $key => $value) {
-                echo  " [$key]";
+                $this->output->stdout(" [$key]");
             }
         }
 
         if (count($options) > 0) {
-            echo  ' [опции]';
+            $this->output->stdout(' [опции]');
         }
 
-        echo PHP_EOL . PHP_EOL;
 
-        echo "\033[34mНазначение:\033[0m" . PHP_EOL;
-        echo $description . PHP_EOL;
+        $this->output->stdout(PHP_EOL . PHP_EOL);
 
-        echo PHP_EOL;
+        $this->output->stdout("\033[34mНазначение:\033[0m" . PHP_EOL);
+        $this->output->stdout($description . PHP_EOL);
+
+        $this->output->stdout(PHP_EOL);
 
         if (count($arguments) > 0) {
-            echo "\033[34mАргументы:\033[0m" . PHP_EOL;
+            $this->output->stdout("\033[34mАргументы:\033[0m" . PHP_EOL);
 
             foreach ($arguments as $key => $argument) {
-                echo "\033[32m$key:\033[0m". ' ' . $argument['description'] . ', ' . ($argument['required'] ? 'обязательный параметр' : 'не обязательный параметр') . PHP_EOL;
+                $this->output->stdout("\033[32m$key:\033[0m". ' ' . $argument['description'] . ', ' . ($argument['required'] ? 'обязательный параметр' : 'не обязательный параметр') . PHP_EOL);
             }
         }
 
-        echo PHP_EOL;
+        $this->output->stdout(PHP_EOL);
 
         if (count($options) > 0) {
-            echo "\033[34mОпции:\033[0m" . PHP_EOL;
+            $this->output->stdout("\033[34mОпции:\033[0m" . PHP_EOL);
 
             foreach ($options as $key => $option) {
-                echo "\033[32m$key:\033[0m". ' ' . $option['description'] . PHP_EOL;
+                $this->output->stdout("\033[32m$key:\033[0m". ' ' . $option['description'] . PHP_EOL);
             }
         }
 
-        echo PHP_EOL;
+        $this->output->stdout(PHP_EOL);
 
         if ($this->input->hasArgument('commandName') === false) {
-            echo "\033[32mДоступные команды:\033[0m" . PHP_EOL;
+            $this->output->stdout("\033[32mДоступные команды:\033[0m" . PHP_EOL);
 
             foreach ($commands as $key => $command) {
                 if ((bool) $command['isHidden'] === true) {
                     continue;
                 }
-                echo "\033[32m$key\033[0m - {$command['description']}" . PHP_EOL;
+                $this->output->stdout("\033[32m$key\033[0m - {$command['description']}" . PHP_EOL);
             }
         }
     }
