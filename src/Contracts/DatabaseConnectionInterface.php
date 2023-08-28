6b48243b8d49d3a03bdc6a465a41199b9bd79972
Фузеев Олег Борисович <o.fuzeev@efko.ru>
2023-08-28 16:43:24 +0300

    [CRALPHAFWK-20230828] добавлен интерфейс соединения с бд

---
+++ src/Contracts/DatabaseConnectionInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Alpha\Contracts;
+
+interface DatabaseConnectionInterface
+{
+    function exec(string $query, array $bindings = []): int;
+
+    function select(string $tableName, array $columns, string $condition = null, array $bindings = []): array|false;
+
+    function selectOne(string $tableName, array $columns, string $condition = null, array $bindings = []): object|false;
+
+    function insert(string $tableName, array $values, string $condition = null, array $bindings = []): int;
+
+    function update(string $tableName, array $values, string $condition = null, array $bindings = []): int;
+
+    function delete(string $tableName, string $condition, array $bindings = []): int;
+}
