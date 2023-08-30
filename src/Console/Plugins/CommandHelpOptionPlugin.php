36ae919b1f218a3f86451b7d43a621efacfa9e81
Хатунцев Никита Андреевич <n.a.hatuntsev@efko.ru>
2023-08-30 17:25:02 +0300

    [CRALPHAFWK-20230830] Удалил сервис для опции --help, перенес функционал в плагин

--- src/Console/Plugins/CommandHelpOptionPlugin.php
+++ src/Console/Plugins/CommandHelpOptionPlugin.php
@@ -2,14 +2,19 @@
 
 namespace Alpha\Console\Plugins;
 
-use Alpha\Console\Components\CommandInfoService;
+use Alpha\Console\CommandDefinition;
 use Alpha\Console\ConsoleKernel;
 use Alpha\Contracts\ConsoleInputInterface;
 use Alpha\Contracts\ConsoleInputPluginInterface;
+use Alpha\Contracts\ConsoleOutputInterface;
 
 class CommandHelpOptionPlugin implements ConsoleInputPluginInterface
 {
-    public function __construct()
+    private CommandDefinition $definition;
+
+    public function __construct(
+        private readonly ConsoleOutputInterface $output
+    )
     {
     }
 
@@ -20,11 +25,48 @@ class CommandHelpOptionPlugin implements ConsoleInputPluginInterface
 
     function handle(ConsoleInputInterface $input): void
     {
-        /** @var CommandInfoService $infoService */
-        $infoService = container()->build(CommandInfoService::class);
-        $infoService->setDefinition($input->getDefinition());
-        $infoService->printCommandInfo();
+        $this->definition = $input->getDefinition();
+        $this->printCommandInfo();
 
         container()->call(ConsoleKernel::class, 'terminate');
+    }
+
+    public function printCommandInfo()
+    {
+        $argsString = '{' . implode('}{', array_keys($this->definition->getArguments())) . '}';
+
+        $optionsString =  $this->definition->getOptions() === [] ? '' : '[опции]';
+        $this->output->info("Вызов:" . PHP_EOL);
+        $this->output->stdout("    " .
+            $this->definition->getCommandName() . " " .
+            $argsString . ' ' .
+            $optionsString.
+            PHP_EOL);
+
+        $this->output->info("Назначение:" . PHP_EOL);
+        $this->output->stdout('    ' . $this->definition->description . PHP_EOL);
+
+        $this->output->info("Аргументы:" . PHP_EOL, 'info');
+        foreach ($this->definition->getArguments() as $name => $argData) {
+            $isRequiredString = $argData['required'] === true ? 'обязательный параметр' : 'необязательный параметр';
+
+            $descriptionOutput = [  $argData['description'], $isRequiredString];
+
+            if($argData['default'] !== null) {
+                $descriptionOutput[] = 'значение по умолчанию: ' . $argData['default'];
+            }
+
+            $this->output->success( '    ' . $name . ' ');
+            $this->output->stdout(  implode(', ' , $descriptionOutput) . PHP_EOL);
+        }
+
+        $this->output->info("Опции:" . PHP_EOL);
+        foreach ($this->definition->getOptions() as $name => $optionData) {
+            if (isset($optionData['isHidden']) && $optionData['isHidden'] === true) {
+                continue;
+            }
+            $this->output->success( '    ' . $name);
+            $this->output->stdout( ' ' . $optionData['description'] . PHP_EOL);
+        }
     }
 }
