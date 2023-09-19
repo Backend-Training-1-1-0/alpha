a97465b678cba6f6d91201f5df6d9f17851a6744
e.nalivkina <e.nalivkina@efko.ru>
2023-09-19 21:45:17 +0300

    реализация объектов Request и Response к виду контрактов Psr\Http\Message\RequestInterface и Psr\Http\Message\ResponseInterface соответственно

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -2,24 +2,135 @@
 
 namespace Alpha\Http;
 
-use Alpha\Contracts\HttpRequestInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+use Psr\Http\Message\StreamInterface;
 
 class Request implements HttpRequestInterface
 {
+    private array $server = [];
+    private array $post = [];
+    private array $get = [];
+
     public function __construct() { }
+
+    public function getMethod(): string
+    {
+        return $this->server['REQUEST_METHOD'] ?? 'GET';
+    }
+
+    public function withMethod($method): RequestInterface
+    {
+        $new = clone $this;
+        $new->server['REQUEST_METHOD'] = $method;
+
+        return $new;
+    }
+
+    public function getRequestTarget(): string
+    {
+        return $this->server['REQUEST_URI'] ?? '/';
+    }
+
+    public function withRequestTarget($requestTarget): RequestInterface
+    {
+        $new = clone $this;
+        $new->server['REQUEST_URI'] = $requestTarget;
+
+        return $new;
+    }
+
+    public function getUri(): UriInterface
+    {
+        // Not implemented
+        return null;
+    }
+
+    public function withUri(UriInterface $uri, $preserveHost = false): RequestInterface
+    {
+        // Not implemented
+        return $this;
+    }
+
+    public function getProtocolVersion(): string
+    {
+        return $this->server['SERVER_PROTOCOL'] ?? '1.1';
+    }
+
+    public function withProtocolVersion($version): RequestInterface
+    {
+        $new = clone $this;
+        $new->server['SERVER_PROTOCOL'] = $version;
+
+        return $new;
+    }
+
+    public function getHeaders(): array
+    {
+        // Not implemented
+        return [];
+    }
+
+    public function hasHeader($name): bool
+    {
+        // Not implemented
+        return false;
+    }
+
+    public function getHeader($name): array
+    {
+        // Not implemented
+        return [];
+    }
+
+    public function getHeaderLine($name): string
+    {
+        // Not implemented
+        return '';
+    }
+
+    public function withHeader($name, $value): RequestInterface
+    {
+        // Not implemented
+        return $this;
+    }
+
+    public function withAddedHeader($name, $value): RequestInterface
+    {
+        // Not implemented
+        return $this;
+    }
+
+    public function withoutHeader($name): RequestInterface
+    {
+        // Not implemented
+        return $this;
+    }
+
+    public function getBody(): StreamInterface
+    {
+        // Not implemented
+        return null;
+    }
+
+    public function withBody(StreamInterface $body): RequestInterface
+    {
+        // Not implemented
+        return $this;
+    }
 
     public function server(): array
     {
-        return $_SERVER;
+        return $this->server;
     }
 
     public function post(): array
     {
-        return $_POST;
+        return $this->post;
     }
 
     public function get(): array
     {
-        return $_GET;
+        return $this->get;
     }
 }
--- src/Http/Response.php
+++ src/Http/Response.php
@@ -2,33 +2,127 @@
 
 namespace Alpha\Http;
 
-use Alpha\Contracts\HttpResponseInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
 
 class Response implements HttpResponseInterface
 {
     private array $headers = [];
-
     private string|null $body = null;
+    private int $statusCode = 200;
+    private string $reasonPhrase = '';
 
     public function __construct() { }
 
     public function send(): never
     {
-        foreach ($this->headers as $key => $value) {
-            header("$key:$value");
+        http_response_code($this->statusCode);
+
+        foreach ($this->headers as $name => $value) {
+            header("$name: $value");
         }
 
         echo $this->body;
+
         exit;
     }
 
-    public function setHeader(string $name, string $header): void
+    public function getProtocolVersion(): string
     {
-        $this->headers[$name] = $header;
+        return '1.1';
     }
 
-    public function setBody(string $body): void
+    public function withProtocolVersion($version): ResponseInterface
     {
-        $this->body = $body;
+        // Not implemented
+        return $this;
+    }
+
+    public function getHeaders(): array
+    {
+        return $this->headers;
+    }
+
+    public function hasHeader($name): bool
+    {
+        return isset($this->headers[$name]);
+    }
+
+    public function getHeader($name): array
+    {
+        if ($this->hasHeader($name)) {
+            return [$this->headers[$name]];
+        }
+
+        return [];
+    }
+
+    public function getHeaderLine($name): string
+    {
+        if ($this->hasHeader($name)) {
+            return $this->headers[$name];
+        }
+
+        return '';
+    }
+
+    public function withHeader($name, $value): ResponseInterface
+    {
+        $new = clone $this;
+        $new->headers[$name] = $value;
+
+        return $new;
+    }
+
+    public function withAddedHeader($name, $value): ResponseInterface
+    {
+        $new = clone $this;
+
+        if ($this->hasHeader($name)) {
+            $new->headers[$name] .= ', ' . $value;
+        } else {
+            $new->headers[$name] = $value;
+        }
+
+        return $new;
+    }
+
+    public function withoutHeader($name): ResponseInterface
+    {
+        $new = clone $this;
+        unset($new->headers[$name]);
+
+        return $new;
+    }
+
+    public function getStatusCode(): int
+    {
+        return $this->statusCode;
+    }
+
+    public function withStatus($code, $reasonPhrase = ''): ResponseInterface
+    {
+        $new = clone $this;
+        $new->statusCode = $code;
+        $new->reasonPhrase = $reasonPhrase;
+
+        return $new;
+    }
+
+    public function getReasonPhrase(): string
+    {
+        return $this->reasonPhrase;
+    }
+
+    public function getBody(): StreamInterface
+    {
+        // Not implemented
+        return null;
+    }
+
+    public function withBody(StreamInterface $body): ResponseInterface
+    {
+        // Not implemented
+        return $this;
     }
 }
